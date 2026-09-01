85d958dfe1bf42efa3c4de33ad6db9afb871e450
shgnposwal-ui <shgnposwal@gmail.com>
2026-09-01 16:11:42 +0530

    Time: 0 ms (100%), Space: 44.5 MB (73.97%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+       int maxwealth=0;
+        
+        for(int i=0;i<accounts.length;i++){
+            int currentwealth=0;
+            for(int j=0;j<accounts[i].length;j++){
+                currentwealth+=accounts[i][j];
+            }
+            maxwealth=Math.max(currentwealth,maxwealth);
+        }
+         return maxwealth;
+        
+        
+    }
+}
